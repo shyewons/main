8005a0dbe2709534d6f4c728cc4941900ecf1c37
shyewons <106545355+shyewons@users.noreply.github.com>
2022-06-01 20:16:53 +0900

    Add files via upload

--- index-script.js
+++ index-script.js
@@ -1,21 +1,24 @@
 window.addEventListener("scroll", function() {
     let scrollHeight = document.documentElement.scrollTop;
-    let menuBtn = document.getElementsByClassName("menu-btn");
+        menuBtn = document.getElementsByClassName("menu-btn");
 
-    let vp = this.innerHeight;
-    let vh = vp;
-    let vh2 = vp*2;
-    let vh3 = vp*3-10;
+        vh = this.innerHeight;
+        vh1 = vh-1
+        vh2min = vh-10;
+        vh2 = vh*2-10;
+        vh3 = vh*3-10;
 
+        underBar = document.getElementsByClassName("under");
 
     for(let i = 0; i < menuBtn.length; i++) {
         menuBtn[i].classList.remove("on");
-        if(scrollHeight >= 0 && scrollHeight <= vh) {
+        if(scrollHeight >= 0 && scrollHeight <= vh1) {
             menuBtn[0].classList.add("on");
-        } else if(scrollHeight > vh && scrollHeight < vh2) {
+        } else if(scrollHeight > vh2min && scrollHeight < vh2) {
             menuBtn[1].classList.add("on");
-        } else if(scrollHeight >= vh2 && scrollHeight < vh3) {
+        } else if(scrollHeight > vh2 && scrollHeight < vh3) {
             menuBtn[2].classList.add("on");
+            underBar[i].classList.add("on");
         } else {
             menuBtn[3].classList.add("on");
         }
